7d6c90da6f00dfe8c4422f784d71a1b88be3cc85
Miguel Camba <miguel.camba@gmail.com>
2014-10-18 01:57:46 +0000

    Ember Bower Auto build for https://github.com/jamesarosen/ember-cpm/commits/756396dbb8993f121b794a4b00fb9f0713ae69fc.

--- dist/named-amd/ember-cpm.js
+++ dist/named-amd/ember-cpm.js
@@ -475,6 +475,24 @@ define("ember-cpm/macros/encode-uri",
     var get = Ember.get;
     var computed = Ember.computed;
 
+    /**
+      Encodes the given string to make it a valid url
+
+      Example
+
+      ```javascript
+      var item = Ember.Object.extend({
+        url: 'http://example.com/one and two'
+        safeUrl: encodeURIComponent('url')
+      }).create();
+
+      item.get('safeUrl'); // 'http://example.com/one%20and%20two'
+      ```
+
+      @method macros.encodeURI
+      @param {String} dependentKey String with the dependent key which value will be encoded.
+      @return {String} A encoded url
+    */
     __exports__["default"] = function EmberCPM_encodeURI(dependentKey) {
       return computed(dependentKey, function(){
         var value = get(this, dependentKey);
