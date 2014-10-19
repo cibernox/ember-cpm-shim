560aff10c55c37d5ed7db78ea9aaeff22c3f6a39
Miguel Camba <miguel.camba@gmail.com>
2014-10-19 00:12:52 +0000

    Ember Bower Auto build for https://github.com/jamesarosen/ember-cpm/commits/336f2f1d17c95b3ccc741f4ca6ab5f7bac78e0bc.

--- dist/cjs/macros/safe-string.js
+++ dist/cjs/macros/safe-string.js
@@ -5,6 +5,13 @@ var get = Ember.get;
 var computed = Ember.computed;
 var EmberHandlebars = Ember.Handlebars;
 
+/**
+  Casts the value of the given dependent key to a Handlebars.SafeString
+
+  @method macros.safeString
+  @param {String} Dependent key with the string to cast.
+  @return {Ember.Handlebars.SafeString} The casted string.
+*/
 exports["default"] = function EmberCPM_safeString(dependentKey) {
 
   return computed(dependentKey, function(){
